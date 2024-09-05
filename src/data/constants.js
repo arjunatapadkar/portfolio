a1781f6b0b78cb048f515f38cb521943d23e4f1d
Arjun Atapadkar <99797400+arjunatapadkar@users.noreply.github.com>
2024-09-05 11:36:09 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,7 +6,7 @@ export const Bio = {
     "Frontend Developer",
     "Backend Developer",
     "Programmer",
-  ],
+  ], 
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/arjunatapadkar",
