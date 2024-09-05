1985a46f8fd8ab4b13c62d5a4442a8e1dae94009
Arjun Atapadkar <99797400+arjunatapadkar@users.noreply.github.com>
2024-09-05 12:17:43 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -37,7 +37,7 @@ const HeroSection = () => {
                     </HeroRightContainer>
                 </HeroInnerContainer>
 
-            </HeroContainer>
+            </HeroContainer> 
         </div>
     )
 }
