1eb17b9c53cc2a7b456d8b5debc976ab1829ae80
Arjun Atapadkar <99797400+arjunatapadkar@users.noreply.github.com>
2024-09-14 19:29:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
           <Projects openModal={openModal} setOpenModal={setOpenModal} />
           <Wrapper>
             <Education />
-            <Contact />
+            // <Contact />
           </Wrapper>
           <Footer />
           {openModal.state &&
